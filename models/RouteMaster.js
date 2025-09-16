41c0cbfc918feada4cb752167ede4edae3918365
Dorian-Cesar <dorian.celu@gmail.com>
2025-09-16 12:35:39 -0300

    add  order to route master

--- models/RouteMaster.js
+++ models/RouteMaster.js
@@ -2,13 +2,15 @@ const mongoose = require('mongoose');
 
 const stopSchema = new mongoose.Schema({
   name: String,
-  offsetMinutes: Number // tiempo acumulado desde inicio
+  offsetMinutes: Number,// tiempo acumulado desde inicio
+  order: Number
 }, { _id: false });
 
 const routeMasterSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
   origin: { type: String, required: true },
   destination: { type: String, required: true },
+  direction: { type: String, enum: ['subida', 'bajada'], required: true },
   stops: [stopSchema], // incluye origen y destino
   layout: { type: mongoose.Schema.Types.ObjectId, ref: 'BusLayout' }
 }, { timestamps: true });
